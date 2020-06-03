d4ce9240cb73d4b867933fed632e96504b535791
unknown <cesar.alvarez.moran@gmail.com>
2020-06-03 16:33:41 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarraComponent } from './barra/barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule
